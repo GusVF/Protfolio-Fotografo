27f22ec23085d43baf4f270fdaae60bf01742fcc
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-16 18:11:03 -0300

    Creating slider component.

--- components/slider.jsx
+++ components/slider.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { SliderData } from './sliderData';
 import Image from 'next/image';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 const Slider = ({slides}) => {
   const [current, setCurrent] = useState(0);
@@ -12,26 +13,40 @@ const Slider = ({slides}) => {
 
   const prevSlide = () => {
     setCurrent(current === 0 ? length - 1 : current - 1)
-  }
+  };
+
+  if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  };
 
   return (
-    <div id='gallery'>
-      <h1>Gallery</h1>
-      <div>
+    <div id='gallery' className="max-w-[1240] mx-auto">
+      <h1 className="text-3xl font-bold text-center p-4">Gallery</h1>
+      <div className="relative flex justify-center p-4">
         {SliderData.map((slide, index) => {
           return(
-            <div key={index} className=''>
-            <Image 
-            src={slide.image} 
-            alt='/'
-            width='1440'
-            height='600'
-            objectFit='cover' 
-            />
-           </div>
+            <div key={index} className={index === current? 'opacity-[1] ease-in duration-1000' : 'opacity-0'}>
+              <FaArrowCircleLeft 
+              className='absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]'
+              size={50} 
+              onClick={prevSlide} />
+            {index === current && (
+              <Image 
+              src={slide.image} 
+              alt='/'
+              width='1440'
+              height='600'
+              objectFit='cover' 
+              />
+            )};
+            <FaArrowCircleRight 
+              className='absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]'
+              size={50} 
+              onClick={prevSlide} />
+              </div>
           );
         })}
-      </div>
+    </div>
     </div> 
   );
 };
