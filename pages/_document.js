d1531d2dcd294c600424ccb582938c8d52b0544d
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-16 12:44:33 -0300

    Installed tailwind, created Hero and navbar.

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital@1&display=swap" rel="stylesheet" />
         </Head>
         <body>
